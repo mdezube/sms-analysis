ce162e56f84ceac2e1dd784540b8e75295a4e6d7
Michael Dezube <mdezube@gmail.com>
2016-05-14 15:09:24 -0400

    Initial check in of sms analysis code.
    See README for details on how to run code.

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # sms-analysis
-Python/IPython code to analyze one's text messages.  Intended to work out of the box, see README for details.
+Python/IPython code to analyze one's text messages.  Intended to work out of the box.
+<br><br>
+Author: Mike Dezube \<michael dezube at gmail dot com\>
+
+# Overview of code
+This code will:
+1. Find your latest iPhone sync (currently only supports doing this automatically on Macs), for PCs edit
+`table_connector.py` to find the file
+2. Load up the messages database and address book database locally
+3. Merge the databases together into `fully_merged_messages_df` which you can freely play with
+4. Visualize a word tree of your text messages with a specific contact
+5. Show you who you text the most
+
+Note: none of your data is modified nor sent anywhere during execution
+
+# Dependencies
+1. [Pandas](http://pandas.pydata.org), see [Pandas install](http://pandas.pydata.org/pandas-docs/stable/install.html)
+2. [IPython](http://ipython.org/) follow Pandas install link for details how to install this as well
+3. An iPhone, having synced with this computer
+4. If running on a Mac, code will work out of the box. If running on a PC, change the variable `BASE_DIR` in
+`table_connector.py` to the directory of your backups
+    * This [post](http://www.iphonefaq.org/comment/70608#comment-70608) seems to specify the location of backups on
+    Windows.
+5. Internet connection to load the google visualization API, it's a very small file though
+
+# Quick start
+1. Start the IPython notebook like so: `jupyter notebook sms_analysis.ipynb`
+2. Under the menu choose Cell --> Run All
+3. Edit the `EXCHANGED_WITH_CONTACT_NAME` and `ROOT_WORD` in the last cell to alter the visualization and then re-run
+that cell, under menu choose: Cell --> Run Cell
+
+Contact michaeldezube at gmail com with questions
+
+Example word tree:
+
+<img height="400" src="https://raw.githubusercontent.com/mdezube/sms-analysis/master/example%20word%20tree.png"></img>
---
+++ table_connector.py
@@ -0,0 +1,203 @@
+"""
+
+Author: Mike Dezube <michael dezube at gmail dot com>
+
+This module abstracts away a lot of the logic of dealing with the sqlite databases that contain text messages and
+contacts on an iPhone.
+
+For further exploring, I highly recommend using sqlitebrowser or DbVisualizer to connect to the databases directly. As
+a convenience, you can find a visualization of both the address book DB and message DB adjacent to this file.
+
+"""
+
+import os
+import pandas as pd
+import re
+import sqlite3
+
+from IPython.display import display
+
+BASE_DIR = '~/Library/Application Support/MobileSync/Backup'
+MESSAGE_DB = '3d0d7e5fb2ce288813306e4d4636395e047a3d28'
+ADDRESS_DB = '31bb7ba8914766d4ba40d6dfb6113c8b614be442'
+
+# START SIMPLE HELPER METHODS
+# --------------
+
+
+# Get's the most recently updated directory within the passed directory.
+def __get_latest_dir_in_dir(directory):
+    directory_path = os.path.expanduser(directory)
+    newest_path, newest_date = ('', -1)
+    for relative_path in os.listdir(directory_path):
+        full_path = os.path.join(directory_path, relative_path)
+        if not os.path.isdir(full_path):  # Ignore non-directories.
+            continue
+        if os.path.getmtime(full_path) > newest_date:
+            newest_path, newest_date = (full_path, os.path.getmtime(full_path))
+    return newest_path
+
+
+latest_sync_dir = __get_latest_dir_in_dir(BASE_DIR)
+print 'Latest directory: {0}'.format(latest_sync_dir)
+message_con = sqlite3.connect('{0}/{1}'.format(latest_sync_dir, MESSAGE_DB))
+address_con = sqlite3.connect('{0}/{1}'.format(latest_sync_dir, ADDRESS_DB))
+
+
+# Removes a leading one if it exists.
+def __strip_initial_1_in_phone(row):
+    phone_or_email = row.phone_or_email
+    return re.sub(r'(\+?1)(\d{10})', r'\2', phone_or_email)
+
+
+# --------------
+# END SIMPLE HELPER METHODS
+
+
+# Joins messages with the phone numbers/emails that sent them.  This handles group messages as well where one
+# message could be sent to multiple people.
+def __get_message_id_joined_to_phone_or_email():
+    message_id_joined_to_phone_or_email = pd.read_sql_query('''
+    SELECT
+      handle.id AS phone_or_email, handle.service, handle.country,
+      chat_message_join.message_id, chat.ROWID AS chat_id
+    FROM handle, chat_handle_join, chat, chat_message_join
+    WHERE
+      handle.ROWID = chat_handle_join.handle_id
+      AND chat_handle_join.chat_id = chat.ROWID
+      AND chat.ROWID = chat_message_join.chat_id''', message_con)
+
+    # Clean it up a bit.
+    message_id_joined_to_phone_or_email['country'] = message_id_joined_to_phone_or_email['country'].str.lower()
+    message_id_joined_to_phone_or_email['phone_or_email'] = message_id_joined_to_phone_or_email.apply(__strip_initial_1_in_phone, axis=1)
+    return message_id_joined_to_phone_or_email
+
+
+# Join the table that has message IDs and phones numbers/emails with the address book in order to get the full
+# name and additional info.
+def __get_address_joined_with_message_id(address_book):
+    address_joined_with_message_id = (
+        __get_message_id_joined_to_phone_or_email().merge(address_book, how='left', left_on='phone_or_email',
+                                                          right_index=True, indicator='merge_chat_with_address')
+    )
+    address_joined_with_message_id = address_joined_with_message_id.drop(['ROWID'], axis=1)
+
+    # Confirm that message_id, chat_id and phone_or_email form a composite key
+    assert sum(address_joined_with_message_id.duplicated(subset=['message_id', 'chat_id', 'phone_or_email'],
+                                                         keep=False)) == 0
+
+    return address_joined_with_message_id
+
+
+def get_message_df():
+    """
+    Loads the message database from disk.
+
+    Returns:
+        a pandas dataframe representing all text messages
+    """
+    messages_df = pd.read_sql_query('''
+    SELECT
+      ROWID as message_id, text, handle_id, country, service, version,
+      DATETIME(date, 'unixepoch', '31 years') AS date,
+      DATETIME(date_read, 'unixepoch', '31 years') AS date_read,
+      DATETIME(date_delivered, 'unixepoch', '31 years') AS date_delivered,
+      is_emote, is_from_me, is_read, is_system_message, is_service_message, is_sent,
+      has_dd_results
+    FROM message''', message_con)
+
+    messages_df = messages_df.set_index(keys='message_id')
+
+    # Convert a few columns to dates.
+    messages_df['date'] = pd.to_datetime(messages_df['date'])
+    messages_df['date_read'] = pd.to_datetime(messages_df['date_read'])
+    messages_df['date_delivered'] = pd.to_datetime(messages_df['date_delivered'])
+
+    # Drop the rows that have no text, I think these are just iphone specific weird rows.
+    messages_df = messages_df.dropna(subset=['text'], how='all')
+    # There seem to be some true duplicates, <100 of them, so just drop them.
+    messages_df = messages_df.drop_duplicates()
+    return messages_df
+
+
+def get_address_book():
+    """
+    Loads the address book database from disk.  Ignores entries that are neither phone numbers or emails.
+
+    Note:
+        This also normalizes phone numbers by removing ()- spaces and a leading 1.
+
+    Returns:
+        a pandas dataframe representing all entries in the address book
+    """
+    address_book = pd.read_sql_query('''
+    SELECT
+      ROWID, ABMultiValue.property, ABMultiValue.value AS phone_or_email,
+      First AS first, Last AS last, Organization AS company,
+      DATETIME(Birthday, 'unixepoch', '31 years') AS birthday,
+      DATETIME(CreationDate, 'unixepoch', '31 years') AS creation_date,
+      DATETIME(ModificationDate, 'unixepoch', '31 years') AS modification_date
+     FROM ABPerson, ABMultiValue
+     WHERE ABPerson.ROWID = ABMultiValue.record_id
+    ''', address_con)
+
+    # Clean it up a bit.
+    address_book = address_book[(address_book['property'] == 4) | (address_book['property'] == 3)]  # Of type phone or email.
+    address_book['phone_or_email'] = address_book['phone_or_email'].str.replace(r'[()\- ]', '')
+    address_book['phone_or_email'] = address_book.apply(__strip_initial_1_in_phone, axis=1)
+
+    # Convert a few columns to dates.
+    address_book['birthday'] = pd.to_datetime(address_book['birthday'], errors='coerce')
+    address_book['creation_date'] = pd.to_datetime(address_book['creation_date'])
+    address_book['modification_date'] = pd.to_datetime(address_book['modification_date'])
+
+    address_book = address_book.set_index('phone_or_email')
+    address_book = address_book.sort_values(by=['first', 'last'])
+
+    return address_book
+
+
+def get_merged_message_df(messages_df, address_book, print_debug=False):
+    """
+        Merges a message dataframe with the address book dataframe to return a single dataframe that contains all
+        messages with detailed information (e.g. name, company, birthday) about the sender.
+
+    Args:
+        messages_df: a dataframe containing all transmitted messages
+        address_book: a dataframe containing the address book as loaded via this module
+        print_debug: true if we should print out the first row of each intermediary table as it's created
+
+    Returns:
+        a dataframe that contained all messages with info about their senders
+    """
+    phones_with_message_id_df = __get_address_joined_with_message_id(address_book)
+
+    if print_debug:
+        print 'Messages Dataframe'
+        display(messages_df.head(1))
+
+        print 'Address Book Dataframe'
+        display(address_book.head(1))
+
+        print 'Phones/emails merged with message IDs via chats Dataframe'
+        display(phones_with_message_id_df.head(1))
+
+    return messages_df.merge(phones_with_message_id_df,
+                             how='left',
+                             suffixes=['_messages_df', '_other_join_tbl'],
+                             left_index=True, right_on='message_id',
+                             indicator='merge_chat_with_address_and_messages')
+
+
+def collapse_first_last_company_columns(df):
+    assert 'first' in df.columns, 'Column "first" did not exist in dataframe'
+    assert 'last' in df.columns, 'Column "last" did not exist in dataframe'
+    assert 'company' in df.columns, 'Column "company" did not exist in dataframe'
+
+    def create_full_name(row):
+        atoms = [atom for atom in [row['first'], row['last'], row['company']] if atom]
+        atoms_as_str = [atom.encode('utf8') if isinstance(atom, unicode) else str(atom) for atom in atoms]
+        return ' '.join(atoms_as_str)
+
+    df['full_name'] = df.apply(create_full_name, axis=1)
+    df.drop(['first', 'last', 'company'], inplace=True, axis=1)
