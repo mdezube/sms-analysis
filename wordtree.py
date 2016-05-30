50dc5d53806e7d6e07ec82b01210bc66c4f99b9d
Michael Dezube <mdezube@gmail.com>
2016-05-30 12:33:07 -0400

    Breaking out the wordtree HTML into its own file.

--- wordtree.py
+++ wordtree.py
@@ -1,11 +1,14 @@
-def get_word_tree_html(json_formatted_for_wordtree, root_word, lowercase=True, tree_type='double'):
+from string import Template
+
+
+def get_word_tree_html(json_formatted, root_word, lowercase=True, tree_type='double'):
     """
         Generates HTML that will draw a word tree using the Google visualization API when rendered.
 
         See https://developers.google.com/chart/interactive/docs/gallery/wordtree for further details.
 
     Args:
-        json_formatted_for_wordtree: JSON to pass directly to the google data visualization API
+        json_formatted: JSON to pass directly to the google data visualization API
         root_word: the word to use as the root of the tree
         lowercase: if true, lowercase the passed texts
         tree_type: the type of word tree, double, suffix, or prefix
@@ -13,63 +16,9 @@ def get_word_tree_html(json_formatted_for_wordtree, root_word, lowercase=True, t
     Returns:
         HTML that will draw a word tree using the Google visualization API when rendered
     """
-    html_blob = """
-    <html>
-      <head>
-        <script type="text/javascript">
-            var chart;
-            var data;
-            var options = {
-                wordtree: {
-                  format: 'implicit',
-                  word: '%s',
-                  type: '%s',
-                }
-            };
 
-            function initChart() {
-                data = google.visualization.arrayToDataTable(%s);
-                chart = new google.visualization.WordTree(document.getElementById('wordtree_basic'));
-            }
-
-            function drawChart() {
-                chart.draw(data, options);
-            }
-
-            function setRootWordFromForm() {
-                options.wordtree.word = document.getElementById('root_word').value;
-                drawChart();
-            }
-
-            // Is it the first time we're being loaded?
-            if (typeof(google) == 'undefined') {
-                var script = document.createElement('script');
-                script.src = 'https://www.gstatic.com/charts/loader.js';
-                var head = document.getElementsByTagName('head')[0];
-                script.onload  = function() {
-                    google.charts.load('current', {packages:['wordtree']});
-                    google.charts.setOnLoadCallback(function() {
-                        initChart();
-                        drawChart();
-                    });
-                    head.removeChild(script);
-                };
-                head.appendChild(script);
-            } else {
-                initChart();
-                drawChart();
-            }
-        </script>
-      </head>
-      <body>
-        Root Word: <input type="text" id="root_word" value="%s">
-        <button type="button" onclick="setRootWordFromForm()">Update Tree</button>
-        <br>
-        <div id="wordtree_basic" style="width: 900px; height: 600px;"></div>
-      </body>
-    </html>
-    """
-    return html_blob % (root_word,
-                        tree_type,
-                        json_formatted_for_wordtree.lower() if lowercase else json_formatted_for_wordtree,
-                        root_word)
+    with open('wordtree_template.html', 'r') as my_file:
+        html_template = Template(my_file.read())
+    return html_template.substitute(root_word=root_word,
+                                    tree_type=tree_type,
+                                    json_formatted=json_formatted.lower() if lowercase else json_formatted)
