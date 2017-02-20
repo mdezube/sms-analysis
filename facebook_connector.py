bae9c8f83fcaf9522ae1002e570e1a1702d3e67b
Nigel Schuster <nigel.schusters@googlemail.com>
2017-02-19 21:41:28 -0500

    Added facebook message parsing

--- facebook_connector.py
+++ facebook_connector.py
@@ -1,5 +1,7 @@
 import os
 import fnmatch
+import pandas as pd
+from fbchat_archive_parser.parser import MessageHtmlParser
 
 def initialize(location = '.'):
     global _message_file
@@ -18,6 +20,22 @@ def initialize(location = '.'):
     if count > 1:
         print 'More than one Message file was found.'
 
+def get_cleaned_fully_merged_messages():
+    parser = MessageHtmlParser(path=_message_file)
+    chats = parser.parse()
+    me = chats.user
+    addr = []
+    msg = []
+    for chat in chats.threads.itervalues():
+        for participant in chat.participants:
+            addr.append([participant])
+        for message in chat.messages:
+            from_me = True if message.sender == me else False
+            msg.append([message.content, message.timestamp, from_me, message.sender])
+    addressbook = pd.DataFrame(columns=("full_name",),data=addr)
+    chatmessage = pd.DataFrame(columns=("text","date","is_from_me","full_name"),data=msg)
+    return chatmessage, addressbook
 
 if __name__ == "__main__":
     initialize()
+    print(get_cleaned_fully_merged_messages())
