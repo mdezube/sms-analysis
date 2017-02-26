45a0206d4f95315686829c3ee445f09e482856cb
Nigel Schuster <nigel.schusters@googlemail.com>
2017-02-26 00:06:41 -0500

    Facebook connector
    * Fixed pep8 issues
    * More descriptive naming
    * Greatly simplified messages.htm lookup logic
    * Other minor tweaks

--- facebook_connector.py
+++ facebook_connector.py
@@ -1,41 +1,40 @@
-import os
-import fnmatch
-import pandas as pd
 from fbchat_archive_parser.parser import MessageHtmlParser
+import fnmatch
+import os
+import pandas as pd
 
-def initialize(location = '.'):
-    global _message_file
+
+def initialize(dump_location='.'):
+    global messages_file
     fb_message_filename = 'messages.htm'
-    _message_file = os.path.join(location, fb_message_filename)
-    if os.path.isfile(_message_file):
+    messages_file = os.path.join(dump_location, fb_message_filename)
+    if not os.path.isfile(messages_file):
+        print("Please place the message.htm file at {}".format(
+            os.path.abspath(messages_file)))
         return
-    count = 0
-    fb_archive_path = 'html'
-    for file in os.listdir(location):
-        if os.path.isdir(os.path.join(location,file)) and fnmatch.fnmatch(file, 'facebook-*'):
-            count = count + 1
-            _message_file = os.path.join(file, fb_archive_path, fb_message_filename)
-    if count == 0:
-        print 'No Message file found.'
-    if count > 1:
-        print 'More than one Message file was found.'
+
 
 def get_cleaned_fully_merged_messages():
-    parser = MessageHtmlParser(path=_message_file)
-    chats = parser.parse()
+    chats = MessageHtmlParser(path=messages_file).parse()
     me = chats.user
-    addr = []
-    msg = []
-    for chat in chats.threads.itervalues():
-        for participant in chat.participants:
-            addr.append([participant])
-        for message in chat.messages:
-            from_me = True if message.sender == me else False
-            msg.append([message.content, message.timestamp, from_me, message.sender])
-    addressbook = pd.DataFrame(columns=("full_name",),data=addr)
-    chatmessage = pd.DataFrame(columns=("text","date","is_from_me","full_name"),data=msg)
-    return chatmessage, addressbook
-
-if __name__ == "__main__":
-    initialize()
-    print(get_cleaned_fully_merged_messages())
+    addresses = set()
+    messages = []
+    for thread in chats.threads.itervalues():
+        for participant in thread.participants:
+            if participant is not None and not participant.isspace():
+                addresses.add(participant)
+        for message in thread.messages:
+            if (message.content is not None
+                and message.content
+                and not message.content.isspace()):
+                from_me = True if message.sender == me else False
+                messages.append([message.content,
+                                 message.timestamp,
+                                 from_me,
+                                 message.sender])
+    address_book = pd.DataFrame(columns=["full_name"],
+                                data=[[address_entry]
+                                      for address_entry in addresses])
+    chat_messages = pd.DataFrame(
+        columns=["text", "date", "is_from_me", "full_name"], data=messages)
+    return chat_messages, address_book
