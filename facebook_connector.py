e1dc384fbd09a40db96e8039a5b584aeb5e64db1
Nigel Schuster <nigel.schusters@googlemail.com>
2017-02-26 18:52:03 -0500

    Facebook Connector
    * Consistent usage of quotes
    * Added compatibility to Python 2 for print statements
    * Formatted code to 100 chars max line length
    * Constistent naming
    * Using BS4 (instead of regex) to get text content of possible HTML document

--- facebook_connector.py
+++ facebook_connector.py
@@ -1,26 +1,24 @@
+from __future__ import print_function
+
+from bs4 import BeautifulSoup
 from fbchat_archive_parser.parser import MessageHtmlParser
 import fnmatch
-from lxml import html
 import os
 import pandas as pd
 import re
-from urllib2 import urlopen
+import warnings
 
 _messages_file = None
-_fb_id_pattern = re.compile("(\\d+)@facebook\\.com")
-_html_tags_pattern = re.compile('<.*?>')
-_mapped_fb_ids = {}
+_fb_id_pattern = re.compile(r"(\d+)@facebook\.com")
 
 
-def initialize(dump_location='.'):
+def initialize(dump_location="."):
     global _messages_file
-    fb_message_filename = 'messages.htm'
+    fb_message_filename = "messages.htm"
     _messages_file = os.path.join(dump_location, fb_message_filename)
     if not os.path.isfile(_messages_file):
-        print("""
-Please place the messages.htm file at {}
-or specify the location of the file in the function call
-""".format(os.path.abspath(_messages_file)))
+        print("Please specify the location of the file in the function call".format(
+            os.path.abspath(_messages_file)))
         _messages_file = None
 
 
@@ -28,9 +26,8 @@ def resolve_user_id(user_id):
     return user_id
 
 
-def get_cleaned_fully_merged_messages(
-                                      resolve_fb_id=False,
-                                      strip_html_content=True):
+def get_cleaned_fully_merged_messages(strip_html_content=True,
+                                      resolve_fb_id=False):
     if _messages_file is None:
         print("Please initialize the module first.")
         return
@@ -38,37 +35,34 @@ def get_cleaned_fully_merged_messages(
     me = chats.user
     addresses = set()
     messages = []
+    warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
     for thread in chats.threads.itervalues():
-        recipients = set()
+        resolved_participants = set()
         for participant in thread.participants:
             if participant is not None and not participant.isspace():
-                recipient = resolve_user_id(
+                resolved_participant = resolve_user_id(
                     participant) if resolve_fb_id else participant
-                recipients.add(recipient)
-        addresses.update(recipients)
+                resolved_participants.add(resolved_participant)
+        addresses.update(resolved_participants)
         for message in thread.messages:
-            if (message.content and not message.content.isspace()):
-                sender = resolve_user_id(
-                    message.sender) if resolve_fb_id else message.sender
-                from_me = sender == me
-                content = ''
-                if strip_html_content:
-                    content = re.sub(_html_tags_pattern, '', message.content)
-                else:
+            if (not message.content or message.content.isspace()):
+                continue
+            sender = resolve_user_id(
+                message.sender) if resolve_fb_id else message.sender
+            from_me = sender == me
+            content = ""
+            if strip_html_content:
+                try:
+                    content = BeautifulSoup(message.content, "html.parser").text
+                except:
                     content = message.content
-                if from_me:
-                    for recipient in recipients:
-                        messages.append([content,
-                                         message.timestamp,
-                                         from_me,
-                                         recipient])
-                else:
-                    messages.append([content,
-                                     message.timestamp,
-                                     from_me,
-                                     sender])
-    address_book_df = pd.DataFrame(columns=["full_name"],
-                                   data=list(addresses))
-    messages_df = pd.DataFrame(
-        columns=["text", "date", "is_from_me", "full_name"], data=messages)
+            else:
+                content = message.content
+            if from_me:
+                for participant in resolved_participants:
+                    messages.append([content, message.timestamp, from_me, participant])
+            else:
+                messages.append([content, message.timestamp, from_me, sender])
+    address_book_df = pd.DataFrame(data=list(addresses), columns=["full_name"])
+    messages_df = pd.DataFrame(data=messages, columns=["text", "date", "is_from_me", "full_name"])
     return messages_df, address_book_df
