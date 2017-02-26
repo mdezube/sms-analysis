0efb7ca62fd39977d5e8b6a3dc64c29f2b7b8c9d
Nigel Schuster <nigel.schusters@googlemail.com>
2017-02-26 17:46:30 -0500

    Facebook connector fixes
    * Added option to resolve numeric ids to names
    * Fixed handling of messages sent by the user himself
    * Fixed several style mistakes

--- facebook_connector.py
+++ facebook_connector.py
@@ -1,40 +1,90 @@
 from fbchat_archive_parser.parser import MessageHtmlParser
 import fnmatch
+from lxml import html
 import os
 import pandas as pd
+import re
+from urllib2 import urlopen
+
+_messages_file = None
+_fb_id_pattern = re.compile("(\\d+)@facebook\\.com")
+_html_tags_pattern = re.compile('<.*?>')
+_mapped_fb_ids = {}
 
 
 def initialize(dump_location='.'):
-    global messages_file
+    global _messages_file
     fb_message_filename = 'messages.htm'
-    messages_file = os.path.join(dump_location, fb_message_filename)
-    if not os.path.isfile(messages_file):
-        print("Please place the message.htm file at {}".format(
-            os.path.abspath(messages_file)))
+    _messages_file = os.path.join(dump_location, fb_message_filename)
+    if not os.path.isfile(_messages_file):
+        print("""
+Please place the messages.htm file at {}
+or specify the location of the file in the function call
+""".format(os.path.abspath(_messages_file)))
+        _messages_file = None
+
+
+def resolve_user_id(user_id):
+    pattern_match = fb_id_pattern.match(user_id)
+    if pattern_match:
+        fb_numeric_id = pattern_match.group(1)
+        try:
+            if fb_numeric_id not in mapped_fb_ids:
+                fb_page_title = html.parse(urlopen(
+                    "https://www.facebook.com/{}".format(fb_numeric_id)
+                    )).find(".//title").text
+                username = fb_page_title.split("|")[0].strip()
+                if not username.startswith('Security Check Required'):
+                    mapped_fb_ids[fb_numeric_id] = username
+            else:
+                return mapped_fb_ids[fb_numeric_id]
+        except:
+            # Probably deleted user
+            mapped_fb_ids[fb_numeric_id] = fb_numeric_id
+    return user_id
+
+
+def get_cleaned_fully_merged_messages(
+                                      resolve_fb_id=False,
+                                      strip_html_content=True):
+    if _messages_file is None:
+        print("Please initialize the module first.")
         return
-
-
-def get_cleaned_fully_merged_messages():
-    chats = MessageHtmlParser(path=messages_file).parse()
+    chats = MessageHtmlParser(path=_messages_file).parse()
     me = chats.user
     addresses = set()
     messages = []
     for thread in chats.threads.itervalues():
+        recipients = set()
         for participant in thread.participants:
             if participant is not None and not participant.isspace():
-                addresses.add(participant)
+                recipient = resolve_user_id(
+                    participant) if resolve_fb_id else participant
+                recipients.add(recipient)
+        addresses.update(recipients)
         for message in thread.messages:
-            if (message.content is not None
-                and message.content
-                and not message.content.isspace()):
-                from_me = True if message.sender == me else False
-                messages.append([message.content,
-                                 message.timestamp,
-                                 from_me,
-                                 message.sender])
-    address_book = pd.DataFrame(columns=["full_name"],
-                                data=[[address_entry]
-                                      for address_entry in addresses])
-    chat_messages = pd.DataFrame(
+            if (message.content and not message.content.isspace()):
+                sender = resolve_user_id(
+                    message.sender) if resolve_fb_id else message.sender
+                from_me = sender == me
+                content = ''
+                if strip_html_content:
+                    content = re.sub(_html_tags_pattern, '', message.content)
+                else:
+                    content = message.content
+                if from_me:
+                    for recipient in recipients:
+                        messages.append([content,
+                                         message.timestamp,
+                                         from_me,
+                                         recipient])
+                else:
+                    messages.append([content,
+                                     message.timestamp,
+                                     from_me,
+                                     sender])
+    address_book_df = pd.DataFrame(columns=["full_name"],
+                                   data=list(addresses))
+    messages_df = pd.DataFrame(
         columns=["text", "date", "is_from_me", "full_name"], data=messages)
-    return chat_messages, address_book
+    return messages_df, address_book_df
