6ae81a6461f58ae50212ccfe134d751b1b4110c6
Nigel Schuster <nigel.schusters@googlemail.com>
2017-03-31 11:21:59 -0400

    Facebook Connector * Added documentation * Minor code cleanup * Removed lxml as requirement

--- facebook_connector.py
+++ facebook_connector.py
@@ -2,7 +2,8 @@
 
 Author: Nigel Schuster <nigel schusters at gmail dot com>
 
-This module provides a simple abstraction for accessing the Facebook chat data
+This module provides a simple abstraction for accessing Facebook messages data
+from a downloaded archive from facebook.com
 
 """
 
@@ -16,12 +17,12 @@ import re
 import warnings
 
 _messages_file = None
-_fb_id_pattern = re.compile(r"(\d+)@facebook\.com")
 
 
 def initialize(dump_directory="."):
     """
-        Initializes the access to the Facebook messages file by asserting its location
+        Asserts the messages.htm file in the Facebook dump can be found,
+        and saves it location for later usage in this module
 
         Args:
             dump_directory: path to the directory that contains messages.htm file
@@ -30,7 +31,7 @@ def initialize(dump_directory="."):
     fb_message_filename = "messages.htm"
     _messages_file = os.path.join(dump_directory, fb_message_filename)
     if not os.path.isfile(_messages_file):
-        print("Please specify the location of the file in the function call")
+        print("The location provided did not contain messages.htm, the location is usually /html.")
         _messages_file = None
 
 
@@ -53,32 +54,32 @@ def resolve_user_id(user_id):
 def get_cleaned_fully_merged_messages(strip_html_content=True,
                                       resolve_fb_id=False):
     """
-    Parses the messages file to create dataframes that contain the messages and their senders
+    Parses the messages file to create dataframes that contain the messages and their senders.
 
     Args:
         strip_html_content: The messages.htm file might contain some html tags in messages; this option will remove all html markup
         resolve_fb_id: The messages.htm file doesn't always print Facebook names, but sometimes ids instead; this will atempt to resolve the issue,
-		but requires a web request per id and is not guaranteed to work
+        but requires a web request per id and is not guaranteed to work
 
     Returns:
         a dataframe that contains all messages with info about their senders
     """
-    if _messages_file is None:
+    if not _messages_file:
         print("Please initialize the module first.")
         return
     chats = MessageHtmlParser(path=_messages_file).parse()
     me = chats.user
     addresses = set()
     messages = []
-    # Suppressing warning on BS4 that shows up when trying to parse non HTML content
-    # Some messages might contain HTML tags others might not and we don't want to display
-    # these warnings to the user
+    # Suppressing warning that BS4 will display when a message only contains a URL
     warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
     try:
         threads = chats.threads.itervalues()
     except AttributeError:
         threads = chats.threads.values()
     for thread in threads:
+        # This set holds the list of participants after their identifier has been
+        # resolved to their name (see  resolve_user_id)
         resolved_participants = set()
         for participant in thread.participants:
             if participant is not None and not participant.isspace():
@@ -102,9 +103,19 @@ def get_cleaned_fully_merged_messages(strip_html_content=True,
                 # If the user is sending a message to a group, then we need to add
                 # one message per group participant to the dataframe
                 for participant in resolved_participants:
-                    messages.append((content, message.timestamp, from_me, participant))
+                    messages.append({
+                        'text': content,
+                        'date': message.timestamp,
+                        'is_from_me': from_me,
+                        'full_name': participant
+                    })
             else:
-                messages.append((content, message.timestamp, from_me, sender))
+                messages.append({
+                    'text': content,
+                    'date': message.timestamp,
+                    'is_from_me': from_me,
+                    'full_name': sender
+                })
     address_book_df = pd.DataFrame(data=list(addresses), columns=["full_name"])
-    messages_df = pd.DataFrame(data=messages, columns=["text", "date", "is_from_me", "full_name"])
+    messages_df = pd.DataFrame.from_records(messages)
     return messages_df, address_book_df
