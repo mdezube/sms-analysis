e473db2706a56dc03e4b440261df08f57cbacb69
Nigel Schuster <Neitsch@users.noreply.github.com>
2017-10-06 01:05:50 -0400

    Add support to use the analysis on Facebook messages
    * Adding Facebook Connector
    * Adding option to resolving FB ids to usernames over the internet

--- facebook_connector.py
+++ facebook_connector.py
@@ -11,17 +11,24 @@ from __future__ import print_function
 
 from bs4 import BeautifulSoup
 from fbchat_archive_parser.parser import MessageHtmlParser
+import logging
 import os
 import pandas as pd
+import re
+import requests
 import warnings
 
+
 _messages_file = None
+
+_FB_ID_PATTERN = re.compile(r"(\d+)@facebook\.com")
+_mapped_fb_ids = {}
 
 
 def initialize(dump_directory="."):
     """
-    Asserts the messages.htm file in the Facebook dump can be found and saves its location for
-    later usage in this module
+    Asserts the messages.htm file in the Facebook dump can be found and
+    saves its location for later usage in this module
 
     Args:
         dump_directory: path to the directory that contains messages.htm file
@@ -30,37 +37,88 @@ def initialize(dump_directory="."):
     fb_message_filename = "messages.htm"
     _messages_file = os.path.join(dump_directory, fb_message_filename)
     if not os.path.isfile(_messages_file):
-        print("The directory provided did not contain messages.htm, the directory is usually "
-              "/html within the archive downloaded from facebook.com")
+        print("""
+            The directory provided did not contain messages.htm,
+            the directory is usually /html within the archive
+            downloaded from facebook.com
+        """)
         _messages_file = None
 
 
-def resolve_user_id(user_id):
+def resolve_user_id(fb_provided_identifier):
     """
-    Tries to map the identifier facebook provides to the name of the user
+    Tries to map the provided identifier for facebook to the name of the user
 
     Args:
-        user_id: identifier string that is provided in the messages file
+        fb_provided_identifier: identifier string that is provided
+            in the messages file. This might be the user's name or
+            his Facebook ID. Only if the input is a Facebook ID in the form
+            of a number followed by @facebook.com, then it will be resolved.
+            For example 100000125040120@facebook.com will resolve to
+            "Nigel Schuster"
 
     Returns:
         The name of the user if it is able to find it, otherwise the input
     """
-    # TODO (neitsch): This method is not implemented yet.  Use the user_id
-    # (format: 12345678@facebook.com) to get the actual name of the user
-    return user_id
+    fb_id_pattern_match = _FB_ID_PATTERN.match(fb_provided_identifier)
+    if not fb_id_pattern_match:
+        # fb_provided_identifier is not in the form
+        # We are expecting (_FB_ID_PATTERN),
+        # so it should not be mapped
+        return fb_provided_identifier
+    fb_numeric_id = fb_id_pattern_match.group(1)
+    if fb_numeric_id in _mapped_fb_ids:
+        # We have looked the id up before, so just return the result
+        return _mapped_fb_ids[fb_numeric_id]
+    try:
+        # Try to find the user id
+        fb_user_page = requests.get(
+            "https://www.facebook.com/{}".format(fb_numeric_id)
+        )
+        fb_page_title = BeautifulSoup(fb_user_page.content).title.string
+        possible_username = fb_page_title.split("|")[0].strip()
+        if ((possible_username.startswith('Security Check Required') or
+             possible_username.startswith('Page Not Found'))):
+            # Mapping not found for this user, this likely is not transient
+            # since the HTTP request validly returned, therefore do not retry
+            _mapped_fb_ids[fb_numeric_id] = fb_numeric_id
+            logging.info(
+                "Failed to lookup {0} via {1}, found result {2}".format(
+                    fb_provided_identifier, fb_numeric_id, fb_page_title
+                )
+            )
+        else:
+            # Here we know that possible_username is the user's name
+            _mapped_fb_ids[fb_numeric_id] = possible_username
+            logging.debug(
+                "Mapped identifier {0} to {1}".format(
+                    fb_provided_identifier, possible_username
+                )
+            )
+    except Exception as e:
+        # Wasn't able to find the user - no harm done
+        _mapped_fb_ids[fb_numeric_id] = fb_numeric_id
+        logging.warning("Ran into error {0} for {1}".format(e, fb_numeric_id))
+    return _mapped_fb_ids[fb_numeric_id]
 
 
 def get_cleaned_fully_merged_messages(strip_html_content=True,
                                       resolve_fb_id=False):
     """
-    Parses the messages file to create dataframes that contain the messages and their senders.
+    Parses the messages file to create dataframes that contain the messages and
+    their senders.
 
     Args:
-        strip_html_content: The messages.htm file might contain some html tags in messages; this
-            option will remove all html markup
-        resolve_fb_id: The messages.htm file doesn't always print Facebook names, but sometimes ids
-            instead; this will attempt to resolve them, but requires a web request per id and is not
-            guaranteed to work
+        strip_html_content: The messages.htm file might contain some html tags
+            in messages; this option will remove all html markup
+        resolve_fb_id: The messages.htm file doesn't always print Facebook
+            names, but sometimes ids instead; this will attempt
+            to resolve them, but requires a web request per id and is not
+            guaranteed to work. Note, that this method will not
+            necessarily succeed, since facebook blocks the number requests
+            above a certain volume threshold. Setting this to true can only
+            improve the results, since it can always fall back to the numeric
+            identifier, but it will increase the time it takes.
 
     Returns:
         a dataframe that contains all messages with info about their senders
@@ -72,15 +130,16 @@ def get_cleaned_fully_merged_messages(strip_html_content=True,
     me = chats.user
     addresses = set()
     messages = []
-    # Suppressing warning that BS4 will display when a message only contains a URL
+    # Suppressing warning that BS4 will display
+    # when a message only contains a URL
     warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
     try:
         threads = chats.threads.itervalues()
     except AttributeError:
         threads = chats.threads.values()
     for thread in threads:
-        # This set holds the list of participants after their identifier has been
-        # resolved to their name (see  resolve_user_id)
+        # This set holds the list of participants after their identifier
+        # has been resolved to their name (see  resolve_user_id)
         resolved_participants = set()
         for participant in thread.participants:
             if participant is not None and not participant.isspace():
@@ -100,8 +159,9 @@ def get_cleaned_fully_merged_messages(strip_html_content=True,
                 content = message.content
             # In the following we add a single message to our dataframe
             if from_me:
-                # If the user is sending a message to a group, then we need to add
-                # one message per group participant to the dataframe
+                # If the user is sending a message to a group,
+                # then we need to add one message
+                # per group participant to the dataframe
                 for participant in resolved_participants:
                     messages.append({
                         'text': content,
